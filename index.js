ab04001e7c5adb729de0c5879d7bd8a9ebe5c56f
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-07-27 12:16:19 +0530

    add Databse connection

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Form server");
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./database/db.js";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+app.get("/", (req, res) => {
+  res.send("server is working");
+});
+app.listen(5000, () => {
+  console.log(`server is running on ${port}`);
+  connectDB();
+});
