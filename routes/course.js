2380564c9d410d01ccfafc5b60488550ae210282
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-07-30 19:53:54 +0530

    add fetch single lecture

--- routes/course.js
+++ routes/course.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { fetchLectures, getAllCourses, getSingleCourses } from "../controllers/course.js";
+import { fetchLectures, getAllCourses, getSingleCourses, fetchLecture } from "../controllers/course.js";
 import { isAuth } from "../middleware/isAuth.js";
 
 const router = express.Router();
 router.get("/course/all", getAllCourses);
 router.get("/course/:id", getSingleCourses);
 router.get("/lecture/:id", isAuth, fetchLectures);
+router.get("/lecture/:id", isAuth, fetchLecture);
 
 export default router;
