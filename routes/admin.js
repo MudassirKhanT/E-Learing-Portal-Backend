54bfdf63c7dac27c9985eca2928ab62e3c214073
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-08-11 14:00:38 +0530

    added admin functions

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAdmin, isAuth } from "../middleware/isAuth.js";
-import { addLectures, createCourse, deleteCourse, deleteLectures, getAllStats } from "../controllers/admin.js";
+import { addLectures, createCourse, deleteCourse, deleteLectures, getAllStats, getAllUsers, updateRole } from "../controllers/admin.js";
 import { uploadFiles } from "../middleware/multer.js";
 
 const router = express.Router();
@@ -9,4 +9,6 @@ router.post("/course/:id", isAuth, isAdmin, uploadFiles, addLectures);
 router.delete("/lecture/:id", isAuth, isAdmin, deleteLectures);
 router.delete("/course/:id", isAuth, isAdmin, deleteCourse);
 router.get("/stats", isAuth, isAdmin, getAllStats);
+router.put("/user/:id", isAuth, isAdmin, updateRole);
+router.get("/users", isAuth, isAdmin, getAllUsers);
 export default router;
